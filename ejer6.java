0f0062d68ecde33f4ec9a3e6009dbc03dcc33655
isaac <115268655+isaac-rub@users.noreply.github.com>
2022-11-07 21:19:55 +0100

    Add files via upload

---
+++ ejer6.java
@@ -0,0 +1,33 @@
+/*
+ */
+import bpc.daw.mario.*;
+import java.util.Scanner;
+/**
+ *
+ * @author usuario-mañana
+ */
+public class ejer6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Mario m2=null;
+        int x;
+        int y;
+         System.out.println("dime dos cordenadas");
+         x=new Scanner(System.in).nextInt();
+         y=new Scanner(System.in).nextInt();
+         m2=new Mario(x,y);
+                 
+         
+         
+        
+        
+        
+        
+        
+    }
+    
+}
