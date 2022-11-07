0443c405b6a1f3657c63c18093a3ca1fad2602cb
isaac <115268655+isaac-rub@users.noreply.github.com>
2022-11-07 20:34:52 +0100

    Add files via upload

---
+++ 02_EjerciciosT2/ejer3.java
@@ -0,0 +1,47 @@
+
+import bpc.daw.mario.*;
+
+
+/*
+ */
+
+/**
+ *
+ * @author usuario-mañana
+ */
+public class ejer3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Mario m1 =null;
+        Luigi l1=null;
+        Seta s1=null;
+        Cañon c1=null;
+        Disparo d1=null;
+        Planta p1=null;
+        
+        m1=new Mario(100,100);
+        m1.saltar();
+        s1=new Seta(200,300);
+        s1.andarHacia(0, 0);
+        c1=new Cañon(100,320);
+        c1.disparar(200, 200);
+        p1=new Planta(400,500);
+        p1.comer(true);
+      p1=new Planta(450,500);
+      p1.comer(true);
+     p1=new Planta(500,500);
+     p1.comer(true);
+     l1=new Luigi(200,200);
+     
+        
+        
+        
+        
+    }
+    
+}
---
+++ 02_EjerciciosT2/ejer4.java
@@ -0,0 +1,25 @@
+/*
+ */
+import bpc.daw.mario.*;
+/**
+ *
+ * @author usuario-mañana
+ */
+public class ejer4 {
+    public static void main(String[] args) {
+    
+    Mario m1 =null;
+    Luigi l1=null;
+    
+    m1=new Mario(500,500);
+    l1=new Luigi(450,500);
+    System.out.println("posicion de mario"+m1.getX()+m1.getY());
+    System.out.println("posicionde luigi"+l1.getX()+l1.getY());
+    
+    
+
+        
+        
+        
+}
+}
