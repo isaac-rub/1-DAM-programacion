ea698c6aadd0914358c875b3eabeef3eb4f39140
isaac <115268655+isaac-rub@users.noreply.github.com>
2022-11-08 17:30:54 +0100

    Add files via upload

---
+++ 02_EjerciciosT2/ejer01.java
@@ -0,0 +1,38 @@
+/*
+ */
+
+/**
+ *
+ * @author usuario-mañana
+ */
+//isaac rubio
+public class t2ejer1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    String radio;
+    String calienta;
+    String enciende;
+    String apagate;
+    String p1="cuanto tiempo puedes estar encendido";
+    String p2="puedes calentar";
+    String p3="puedes gratinar";
+    //p1 int
+    //p2 boolean
+    //p3 boolean
+    
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
---
+++ 02_EjerciciosT2/ejer09.java
@@ -0,0 +1,29 @@
+/*
+ */
+import bpc.daw.objetos.*;
+/**
+ *
+ * @author usuario-mañana
+ */
+public class ejer9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+      Caja c1=null;  
+      c1=new Caja("bienvenidos al instituto");
+      System.out.println(c1.getMensaje());
+      c1.abrirCaja();
+      System.out.println(c1.getMensaje());
+      
+      
+        
+        
+        
+        
+    }
+    
+}
