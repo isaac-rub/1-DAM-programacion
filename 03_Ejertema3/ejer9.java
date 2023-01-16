0b41afd08ff6731341461ea5cba9068b75be6248
isaac <115268655+isaac-rub@users.noreply.github.com>
2023-01-16 20:10:11 +0100

    Add files via upload

---
+++ 03_Ejertema3/ejer9.java
@@ -0,0 +1,29 @@
+package unidad3ejer;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author usuario-mañana
+ */
+public class ejer9 {
+
+    public static void main(String[] args) {
+
+        double s = 1;
+        double a = 1;
+        int n;
+
+        System.out.println("dame un numero entero pasitivo");
+        n = new Scanner(System.in).nextInt();
+        s = (1 / 2) * (a + n / a);
+        System.out.println(s);
+        if (s - a <= 0.01) {
+            System.out.println("emos terminado");
+        } else {
+            a = s;
+            System.out.println(s - a);
+        }
+    }
+
+}
